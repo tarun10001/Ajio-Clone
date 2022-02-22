bb07e73bcb04d0d97cbd9af612dd231112376152
naszifnaaz <naszifnaaz@gmail.com>
2022-02-22 23:21:27 +0530

    added-sort-functionality

--- scripts/renderProducts.js
+++ scripts/renderProducts.js
@@ -1,24 +1,42 @@
 // Rendering Products to DOM
 function renderProducts(data) {
+    let parent = document.getElementById("naz-product-container");
+    parent.innerHTML = "";
     data.map((elem) => {
         let product = document.createElement("div");
-        product.setAttribute("class", "product");
+        product.setAttribute("class", "naz-product");
 
         let poster = document.createElement("img");
-        poster.setAttribute("class", "product-img");
+        poster.setAttribute("class", "naz-product-img");
         poster.setAttribute("src", elem.imageURL);
 
         let brand = document.createElement("p");
-        brand.setAttribute("class", "product-brand");
+        brand.setAttribute("class", "naz-product-brand");
         brand.innerHTML = elem.brand;
 
         let title = document.createElement("p");
-        title.setAttribute("class", "product-title");
+        title.setAttribute("class", "naz-product-title");
         title.innerHTML = elem.product;
 
-        product.append(poster, brand, title);
+        let price_container = document.createElement("div");
+        price_container.setAttribute("class","naz-price-container");
 
-        let parent = document.getElementById("naz-product-container");
+        let price = document.createElement("p");
+        price.setAttribute("class","naz-price");
+        price.innerHTML = `₹${elem.price}`;
+
+        let strikedPrice = document.createElement("p");
+        strikedPrice.setAttribute("class","naz-striked-price");
+        strikedPrice.innerHTML = `₹${elem.strikedOffPrice}`;
+
+        let offer = document.createElement("p");
+        offer.setAttribute("class","naz-offer");
+        let offerValue = Math.floor(((elem.strikedOffPrice - elem.price)/elem.strikedOffPrice) * 100);
+        offer.innerHTML = `(${offerValue}% off)`;
+
+        price_container.append(price,strikedPrice,offer);
+        product.append(poster, brand, title,price_container);
+
         parent.append(product);
     });
 }
--- scripts/womenStyles.js
+++ scripts/womenStyles.js
@@ -5,3 +5,25 @@ let data = JSON.parse(localStorage.getItem("womensData"));
 import renderProducts from "./renderProducts.js";
 
 renderProducts(data);
+
+//Sort functionality
+document.getElementById("naz-sort-products").addEventListener("change", sortByPrice);
+function sortByPrice() {
+    let selected = document.getElementById("naz-sort-products-option").value;
+
+    if(selected == "lowtohigh")
+    {
+        data.sort((a,b)=>{
+            return a.price - b.price;
+        });
+        renderProducts(data);
+    }
+
+    if(selected == "hightolow")
+    {
+        data.sort((a,b)=>{
+            return b.price - a.price;
+        });
+        renderProducts(data);
+    }
+}
