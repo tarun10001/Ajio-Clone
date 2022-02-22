f3d444852ad2fd0652dc663d4bb37fc2d75cbcc7
naszifnaaz <naszifnaaz@gmail.com>
2022-02-22 20:13:08 +0530

    Added Men,Women,Kids Data

---
+++ assets/kidData.js
@@ -0,0 +1,157 @@
+// Kids Styles
+
+let kidData = [
+    {
+        imageURL:
+            "https://assets.ajio.com/medias/sys_master/root/20220222/pmBZ/6214007daeb26921afa7dec1/cherry_crumble_by_nitt_hyman_red_checked_shirt_with_flap_pockets.jpg",
+        brand: "NITT HYMAN",
+        product: "Checked Shirt with Flap Pockets",
+        gender: "boy",
+        category: "kids",
+        price: 1109,
+        strikedOffPrice: 1849,
+    },
+    {
+        imageURL:
+            "https://assets.ajio.com/medias/sys_master/root/20210831/8r7g/612e23547cdb8cb824103117/noddy_multicoloured_colourblock_crew-neck_t-shirt_with_applique.jpg",
+        brand: "NODDY",
+        product: "Colourblock Crew-Neck T-shirt with Applique",
+        gender: "boy",
+        category: "kids",
+        price: 358,
+        strikedOffPrice: 550,
+    },
+    {
+        imageURL:
+            "https://assets.ajio.com/medias/sys_master/root/20220122/Xh3W/61eb1a38aeb2695cdd28af2d/max_multicoloured_checked_shirt_with_patch_pocket_.jpg",
+        brand: "MAX",
+        product: "Checked Shirt with Patch Pocket",
+        gender: "boy",
+        category: "kids",
+        price: 549,
+        strikedOffPrice: 699,
+    },
+    {
+        imageURL:
+            "https://assets.ajio.com/medias/sys_master/root/20220207/8Uek/6201389baeb26921af8c7ea3/poplins_brown_%26_white_floral_print_print_top_with_pants_.jpg",
+        brand: "POPLINS",
+        product: "Floral Print Print Top with Pants",
+        gender: "girl",
+        category: "kids",
+        price: 650,
+        strikedOffPrice: 1299,
+    },
+    {
+        imageURL:
+            "https://assets.ajio.com/medias/sys_master/root/20220211/uedG/6206584caeb26921af93d377/tommy_hilfiger_yellow_brand_print_round-neck_organic_cotton_t-shirt.jpg",
+        brand: "TOMMY HILFIGER",
+        product: "Brand Print Round-Neck Organic Cotton T-shirt",
+        gender: "boy",
+        category: "kids",
+        price: 1299,
+        strikedOffPrice: 1499,
+    },
+    {
+        imageURL:
+            "https://assets.ajio.com/medias/sys_master/root/20220113/HXSd/61df3fb7aeb2695cdd12271f/peppermint_maroon_%26_white_micro_printed_fit_and_flare_frocks.jpg",
+        brand: "PEPPERMINT",
+        product: "Micro Printed Fit and Flare Frocks",
+        gender: "girl",
+        category: "kids",
+        price: 1055,
+        strikedOffPrice: 1599,
+    },
+    {
+        imageURL:
+            "https://assets.ajio.com/medias/sys_master/root/20220111/02kd/61dc989bf997dd662318a27f/disney_by_wear_your_mind_black_cartoon_round_neck_t-shirt.jpg",
+        brand: "DISNEY",
+        product: "Cartoon Round Neck T-shirt",
+        gender: "girl",
+        category: "kids",
+        price: 383,
+        strikedOffPrice: 1095,
+    },
+    {
+        imageURL:
+            "https://assets.ajio.com/medias/sys_master/root/20220207/p3Wq/620112c3aeb26921af8bb430/kb_team_spirit_blue_printed_crew-neck_t-shirt_with_striped_shirt.jpg",
+        brand: "KB TEAM SPIRIT",
+        product: "Printed Crew-Neck T-shirt with Striped Shirt",
+        gender: "boy",
+        category: "kids",
+        price: 649,
+        strikedOffPrice: 999,
+    },
+    {
+        imageURL:
+            "https://assets.ajio.com/medias/sys_master/root/20211231/AWpf/61cf1b11f997dd66230bfffb/rio_girls_pink_graphic_print_crew-neck_t-shirt.jpg",
+        brand: "RIO GIRLS",
+        product: "Graphic Print Crew-Neck T-shirt",
+        gender: "girl",
+        category: "kids",
+        price: 199,
+        strikedOffPrice: 249,
+    },
+    {
+        imageURL:
+            "https://assets.ajio.com/medias/sys_master/root/20220113/gHCk/61df3fa3f997dd66231e6905/peppermint_peach_metallic_print_a-line_dress_with_floral_applique.jpg",
+        brand: "PEPPERMINT",
+        product: "Metallic Print A-line Dress with Floral Applique",
+        gender: "girl",
+        category: "kids",
+        price: 857,
+        strikedOffPrice: 1299,
+    },
+    {
+        imageURL:
+            "https://assets.ajio.com/medias/sys_master/root/20211111/GTbi/618d303aaeb2690110c0ed9b/max_red_camouflage_typographic_print_t-shirt_with_doctor_sleeves.jpg",
+        brand: "MAX",
+        product: "Camouflage Typographic Print T-shirt ",
+        gender: "boy",
+        category: "kids",
+        price: 369,
+        strikedOffPrice: 499,
+    },
+    {
+        imageURL:
+            "https://assets.ajio.com/medias/sys_master/root/20220111/ZOmg/61dca4ccaeb2695cdd0d9e44/max_red_graphic_print_round-neck_t-shirt.jpg",
+        brand: "MAX",
+        product: "Graphic Print Round-Neck T-shirt",
+        gender: "boy",
+        category: "kids",
+        price: 149,
+        strikedOffPrice: 199,
+    },
+    {
+        imageURL:
+            "https://assets.ajio.com/medias/sys_master/root/20220210/JyHe/620424bef997dd03e2c8cacd/max_multi__v-neck_t-shirt_with_patch_pocket.jpg",
+        brand: "MAX",
+        product: "V-Neck T-shirt with Patch Pocket",
+        gender: "boy",
+        category: "kids",
+        price: 299,
+        strikedOffPrice: 349,
+    },
+    {
+        imageURL:
+            "https://assets.ajio.com/medias/sys_master/root/20220215/pMhZ/620bc610aeb26921af99f88b/inf_frendz_blue_%26_white_placement_graphic_print_colourblock_shirt.jpg",
+        brand: "INF FRENDZ",
+        product: "Placement Graphic Print Colourblock Shirt",
+        gender: "boy",
+        category: "kids",
+        price: 399,
+        strikedOffPrice: 449,
+    },
+    {
+        imageURL:
+            "https://assets.ajio.com/medias/sys_master/root/20211111/tuQs/618c20e7aeb2690110c0aa65/max_red_graphic_print_round-neck_top.jpg",
+        brand: "MAX",
+        product: "Graphic Print Round-Neck Top",
+        gender: "girl",
+        category: "kids",
+        price: 329,
+        strikedOffPrice: 399,
+    }
+];
+
+// Writing Womens Data to Local Storage
+localStorage.setItem("kidsData",JSON.stringify(kidData));
---
+++ assets/womenData.js
@@ -0,0 +1,216 @@
+// Women Data Styles
+let womenData = [
+    {
+        imageURL:
+            "https://assets.ajio.com/medias/sys_master/root/20220127/RXhg/61f2daaef997dd66234079f3/gosriki_pink_geometric_print_straight_kurta_set.jpg",
+        brand: "GOSRIKI",
+        product: "Geometric Print Straight Kurta Set",
+        gender: "female",
+        category: "kurta",
+        price: 895,
+        strikedOffPrice: 3196,
+    },
+    {
+        imageURL:
+            "https://assets.ajio.com/medias/sys_master/root/20220124/8ATC/61eeae7af997dd662337baf6/gulmohar_jaipur_yellow_floral_printed_straight_kurta_set.jpg",
+        brand: "GULMOHAR JAIPUR",
+        product: "Floral Printed Straight Kurta Set",
+        gender: "female",
+        category: "kurta",
+        price: 1199,
+        strikedOffPrice: 2499,
+    },
+    {
+        imageURL:
+            "https://assets.ajio.com/medias/sys_master/root/20220124/IYjE/61eeadd8f997dd662337b165/gulmohar_jaipur_maroon_floral_print_straight_kurta_set.jpg",
+        brand: "GULMOHAR JAIPUR",
+        product: "Floral Print Straight Kurta Set",
+        gender: "female",
+        category: "kurta",
+        price: 1199,
+        strikedOffPrice: 2499,
+    },
+    {
+        imageURL:
+            "https://assets.ajio.com/medias/sys_master/root/20211006/ntzG/615d2b66f997ddce89026c9f/ishoo_white_polka-dot_flared_kurta_set.jpg",
+        brand: "ISHOO",
+        product: "Polka-dot Flared Kurta Set",
+        gender: "female",
+        category: "kurta",
+        price: 959,
+        strikedOffPrice: 1599,
+    },
+    {
+        imageURL:
+            "https://assets.ajio.com/medias/sys_master/root/20220124/zTEx/61eeac9bf997dd6623379e6d/gulmohar_jaipur_teal_floral_print_flared_kurta_set.jpg",
+        brand: "GULMOHAR JAIPUR",
+        product: "Floral Print Flared Kurta Set",
+        gender: "female",
+        category: "kurta",
+        price: 1299,
+        strikedOffPrice: 2499,
+    },
+    {
+        imageURL:
+            "https://assets.ajio.com/medias/sys_master/root/20211125/cMM7/619fbe5aaeb2690110d8b766/gosriki_pink_floral_printed_straight_kurta_sets.jpg",
+        brand: "GOSRIKI",
+        product: "Floral Printed Straight Kurta Sets",
+        gender: "female",
+        category: "kurta",
+        price: 895,
+        strikedOffPrice: 3401,
+    },
+    {
+        imageURL:
+            "https://assets.ajio.com/medias/sys_master/root/20210316/YU0Y/604fb60bf997dd5c40116be6/avaasa_mix_n%27_match_yellow_floral_print_straight_kurta.jpg",
+        brand: "AVAASA MIX N' MATCH",
+        product: "Floral Print Straight Kurta",
+        gender: "female",
+        category: "kurta",
+        price: 349,
+        strikedOffPrice: 499,
+    },
+    {
+        imageURL:
+            "https://assets.ajio.com/medias/sys_master/root/20210615/vaXX/60c7f988f997ddb312cac345/gosriki_blue_floral_print_straight_kurta_set.jpg",
+        brand: "GOSRIKI",
+        product: "Floral Print Straight Kurta Set",
+        gender: "female",
+        category: "kurti",
+        price: 657,
+        strikedOffPrice: 3051,
+    },
+    {
+        imageURL:
+            "https://assets.ajio.com/medias/sys_master/root/20220107/8BhM/61d75260f997dd662313585b/anubhutee_blue_indian__anarkali_kurta_set.jpg",
+        brand: "ANUBHUTEE",
+        product: "Indian Anarkali Kurta Set",
+        gender: "female",
+        category: "kurti",
+        price: 1485,
+        strikedOffPrice: 4499,
+    },
+    {
+        imageURL:
+            "https://assets.ajio.com/medias/sys_master/root/20210404/cf7C/606beceaaeb269a9e36fc13c/aks_green_checked_straight_kurta.jpg",
+        brand: "AKS",
+        product: "Checked Straight Kurta",
+        gender: "female",
+        category: "kurti",
+        price: 450,
+        strikedOffPrice: 999,
+    },
+    {
+        imageURL:
+            "https://assets.ajio.com/medias/sys_master/root/20210430/hG7l/608c1bd1aeb269a9e3a65fb8/fusion_white_printed_straight_kurta_with_high-low_hemline.jpg",
+        brand: "FUSION",
+        product: "Printed Straight Kurta with High-Low Hemline",
+        gender: "female",
+        category: "kurta",
+        price: 349,
+        strikedOffPrice: 499,
+    },
+    {
+        imageURL:
+            "https://assets.ajio.com/medias/sys_master/root/20220114/bhqJ/61e16cd6aeb2695cdd157ae9/avaasa_mix_n%27_match_burgundy_straight_kurta_with_floral_embroidered_yoke.jpg",
+        brand: "AVAASA MIX N' MATCH",
+        product: "Straight Kurta with Floral Embroidered Yoke",
+        gender: "female",
+        category: "kurta",
+        price: 424,
+        strikedOffPrice: 499,
+    },
+    {
+        imageURL:
+            "https://assets.ajio.com/medias/sys_master/root/20220122/kmHz/61eb25e8aeb2695cdd2966a1/fable_street_navy_solid_v-neck__top.jpg",
+        brand: "FABLE STREET",
+        product: "Solid V-Neck Top",
+        gender: "female",
+        category: "top",
+        price: 1995,
+        strikedOffPrice: 2199,
+    },
+    {
+        imageURL:
+            "https://assets.ajio.com/medias/sys_master/root/20220201/iHwp/61f8d5bcaeb26921af836d75/orchid_blues_black_joggers_with_insert_pockets.jpg",
+        brand: "ORCHID BLUES",
+        product: "Joggers with Insert Pockets",
+        gender: "female",
+        category: "leggins",
+        price: 520,
+        strikedOffPrice: 1299,
+    },
+    {
+        imageURL:
+            "https://assets.ajio.com/medias/sys_master/root/20220211/zqWG/62068100f997dd03e2cc5b14/tommy_hilfiger_red_printed_slim_fit_polo_t-shirt.jpg",
+        brand: "TOMMY HILFIGER",
+        product: "Printed Slim Fit Polo T-shirt",
+        gender: "female",
+        category: "top",
+        price: 2799,
+        strikedOffPrice: 3199,
+    },
+    {
+        imageURL:
+            "https://assets.ajio.com/medias/sys_master/root/20210528/0ps8/60b11029f997ddb312ad2abd/dnmx_blue_washed_slim_fit_jeans.jpg",
+        brand: "DNMX",
+        product: "Washed Slim Fit Jeans",
+        gender: "female",
+        category: "leggins",
+        price: 899,
+        strikedOffPrice: 999,
+    },
+    {
+        imageURL:
+            "https://assets.ajio.com/medias/sys_master/root/20220211/IKH0/62067de9f997dd03e2cc2dd2/tommy_hilfiger_purple_crew-neck_t-shirt.jpg",
+        brand: "TOMMY HILFIGER",
+        product: "Crew-Neck T-shirt",
+        gender: "female",
+        category: "top",
+        price: 1999,
+        strikedOffPrice: 2299,
+    },
+    {
+        imageURL:
+            "https://assets.ajio.com/medias/sys_master/root/20211026/OzFC/6177136daeb2690110a91cb5/nike_black_skinny_fit_track_pants_with_slip_pockets.jpg",
+        brand: "NIKE",
+        product: "Skinny Fit Track Pants with Slip Pockets",
+        gender: "female",
+        category: "leggins",
+        price: 2660,
+        strikedOffPrice: 3695,
+    },
+    {
+        imageURL:
+            "https://assets.ajio.com/medias/sys_master/root/20210710/6cuZ/60e8c293aeb269a9e33f17e5/soch_black_floral_print_georgette_saree.jpg",
+        brand: "SOCH",
+        product: "Floral Print Georgette Saree",
+        gender: "female",
+        category: "saree",
+        price: 1749,
+        strikedOffPrice: 2498,
+    },
+    {
+        imageURL:
+            "https://assets.ajio.com/medias/sys_master/root/20210706/P5HT/60e38074aeb269a9e3388c95/buta_buti_orange_floral_print_cotton_saree.jpg",
+        brand: "BUTA BUTI",
+        product: "Floral Print Cotton Saree",
+        gender: "female",
+        category: "saree",
+        price: 1399,
+        strikedOffPrice: 1999,
+    },
+    {
+        imageURL:
+            "https://assets.ajio.com/medias/sys_master/root/20211225/oySa/61c63f8caeb26901101c7618/marziyaa_peach_%26_green_kanjeevaram_meenakari_traditional_saree_.jpg",
+        brand: "MARZIYAA",
+        product: "Kanjeevaram Meenakari Traditional Saree",
+        gender: "female",
+        category: "saree",
+        price: 1500,
+        strikedOffPrice: 2999,
+    }
+];
+
+// Writing Womens Data to Local Storage
+localStorage.setItem("womensData",JSON.stringify(womenData));
